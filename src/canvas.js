13534d7d6f7682c20e73e242e12d7013569533c1
jacekk <github.com@jacek-k.net>
2018-01-10 00:41:14 +0100

    added circle following mouse;

--- src/canvas.js
+++ src/canvas.js
@@ -1,20 +1,19 @@
 // Initial Setup
 const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const ctx = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-// Variables
+// Variables / Constants
 const mouse = {
     x: innerWidth / 2,
-    y: innerHeight / 2
+    y: innerHeight / 2,
 }
-
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+// const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 
 // Event Listeners
-addEventListener('mousemove', event => {
+addEventListener('mousemove', (event) => {
     mouse.x = event.clientX
     mouse.y = event.clientY
 })
@@ -27,6 +26,7 @@ addEventListener('resize', () => {
 })
 
 // Utility Functions
+/*
 function randomIntFromRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
@@ -41,43 +41,72 @@ function distance(x1, y1, x2, y2) {
 
     return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
 }
+*/
 
 // Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+function MouseCircle() {
+    this.x = mouse.x
+    this.y = mouse.y
+    this.radius = 60
 }
 
-Object.prototype.update = function() {
+MouseCircle.prototype.calculateNewCoord = function(prop) {
+    const mousePoint = mouse[prop]
+
+    if (Math.round(this[prop]) === mousePoint) {
+        return
+    }
+
+    const diff = Math.floor(mousePoint - this[prop]) / 15
+
+    if (Math.abs(diff) > 0.05) {
+        this[prop] += diff
+    } else {
+        this[prop] = mousePoint
+    }
+}
+
+MouseCircle.prototype.update = function() {
+    this.calculateNewCoord('x')
+    this.calculateNewCoord('y')
     this.draw()
 }
 
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
+MouseCircle.prototype.draw = function() {
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.15)'
+    ctx.strokeStyle = 'rgba(255, 0, 0, 0.3)'
+    ctx.lineWidth = 4
+
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fill()
+    ctx.stroke()
+    ctx.closePath()
 }
 
 // Implementation
-let objects
-function init() {
-    objects = []
+let circles
+let mouseCircle
 
-    for (let i = 0; i < 400; i++) {
-        // objects.push();
+function init() {
+    mouseCircle = new MouseCircle()
+    circles = []
+
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 6; j++) {
+            // circles.push();
+        }
     }
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
 
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    mouseCircle.update()
+
+    // ctx.fillText("HTML CANVAS BOILERPLATE", mouse.x, mouse.y);
     // objects.forEach(object => {
     //  object.update();
     // });
