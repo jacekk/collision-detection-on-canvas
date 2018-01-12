61e81ff050139f403c4e8ba3f0b5db99a85ec1b9
jacekk <github.com@jacek-k.net>
2018-01-12 18:02:36 +0100

    added prod build;

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,41 @@
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const webpack = require('webpack')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const plugins = []
+
+if (!isProduction) {
+    plugins.push(
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['dist'] },
+            files: ['./dist/*'],
+        })
+    )
+}
 
 module.exports = {
     entry: './src/canvas.js',
     output: {
         path: __dirname + '/dist/js',
-        filename: 'canvas.bundle.js'
+        filename: 'bundle.js',
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env']
-                }
-            }
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    },
+                },
+            },
+        ],
     },
-    plugins: [
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 3000,
-            server: { baseDir: ['dist'] },
-            files: ['./dist/*']
-        }),
-    ],
-    watch: true,
-    devtool: 'source-map'
-};
+    plugins,
+    watch: !isProduction,
+    devtool: isProduction ? false : 'source-map',
+}
